bfdc2fcce98055460f0d2b47d7f88c93b366a51a
Denilson Gomes <denilsongomesdg97@gmail.com>
2018-09-06 17:57:57 -0300

    Projeto pronto pra entrega e comentado

---
+++ itens679.m
@@ -0,0 +1,4 @@
+% Os itens 6,7 e 9 sao obtidos com pequenas alterações nos scripts ja feitos,
+% logo nao serao refeitos, muito em questao do tempo.
+% A analise dos resultados dessas mudanças levaria tambem mais tempo, portanto
+% nao esta descrita aki, por enquanto.
