2d77254f97f94050275990266a858174a4fa399f
Denilson Gomes <denilsongomesdg97@gmail.com>
2018-09-06 14:36:35 -0300

    Codigo finalizado e comentado

--- somaSenos.m
+++ somaSenos.m
@@ -6,5 +6,5 @@
 
 n = 0:1:200; %Numero de pontos
 w1 = 0.1*pi; w2 = 0.5*pi; w3 = 0.75*pi; %Frequencias
-x = sin(n*w1) + sin(n*w2) + sin(n*w3);
-stem(x)
+x = sin(n*w1) + 1.5*sin(n*w2) + 0.5*sin(n*w3); %Gera o sinal que é a soma dos 3 senos com suas frequencias
+stem(x) %Plota o sinal
