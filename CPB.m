f4a4f2c4e3055ecc76fa8f65351de8f2aaf241bc
Denilson Gomes <denilsongomesdg97@gmail.com>
2018-09-06 16:37:19 -0300

    Codigo finalizado e comentado

--- CPB.m
+++ CPB.m
@@ -9,5 +9,14 @@ wc1 = 0.3*pi; %Frquencia de conrte
 [B,A] = butter(4, wc1/pi) %Coeficientes do filtro PB
 [H,W] = freqz(B,A,length(x));
 figure,plot(W,abs(H)) %Plota filtro Passa-Baixa
-ylabel('Aceitação do sinal') %legenda eixo y
+ylabel('Modulo do filtro') %legenda eixo y
+xlabel('Frequencia') %legenda eixo x
+
+figure,plot(W,angle(H)) %Plota filtro Passa-Baixa
+ylabel('Fase do filtro') %legenda eixo y
+xlabel('Frequencia') %legenda eixo x
+
+[h,t] = impz(H);
+figure,plot(t,h) %Plota filtro Passa-Baixa
+ylabel('Resposta ao Impulso') %legenda eixo y
 xlabel('Frequencia') %legenda eixo x
