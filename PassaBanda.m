0863b1d548709a65a38a08310736980b02a9ffb2
Denilson Gomes <denilsongomesdg97@gmail.com>
2018-09-07 17:47:07 -0300

    Codigo finalizado e comentado

--- PassaBanda.m
+++ PassaBanda.m
@@ -4,11 +4,31 @@
 %Prof. Dr. Carlos Alexandre Rolim Fermamdes
 %Codigo para implementar filtro passa-banda
 
-% Filtragem
+stem(x) %Plotagem do sinal antes de aplicar o filtro
+ylabel('Amplitude do Sinal')
+xlabel('Tempo')
+
+% Filtro
 wc1 = 0.3*pi; %Frquencia inicial
 wc2 = 0.6*pi; %Frquencia final
 [B,A] = butter(4, [wc1/pi,wc2/pi]); %Coeficientes do filtro PB
 [H,W] = freqz(B,A,length(x));
-figure,plot(W,abs(H)) %Plota o modulo do filtro Passa-Baixa
-ylabel('Modulo do filtro') %legenda eixo y
+figure,plot(W,abs(H)) %Plota o modulo do filtro Passa-Banda
+ylabel('Modulo do Filtro') %legenda eixo y
 xlabel('Frequencia') %legenda eixo x
+
+%Filtrar o sinal
+x_filt = filter(B,A,x); %Filtra o sinal com ruido
+figure,plot(n,x_filt) %Plota o sinal filtrado
+ylabel('Amplitude do Sinal Filtrado')
+xlabel('Tempo')
+
+Xw = fftshift(fft(x)); %Transformada de Fourier do sinal filtrado
+figure,plot(freq_vec,abs(Xw)) %plota o modulo do sinal filtrado
+ylabel('Espectro de magnitude do sinal')
+xlabel('Frequencia')
+
+Xw_filtrado = fftshift(fft(x_filt)); %Transformada de Fourier do sinal filtrado
+figure,plot(freq_vec,abs(Xw_filtrado)) %plota o modulo do sinal filtrado
+ylabel('Espectro de magnitude do sinal filtrado')
+xlabel('Frequencia')
